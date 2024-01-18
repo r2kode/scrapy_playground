215822c1babd88e839b158aa7f71ac3b4ff0fdd0
r2k <r2kodepl@gmail.com>
2024-01-18 14:24:29 +0100

    feat: add middleware for faking headers or useragents

--- bookscraper/bookscraper/settings.py
+++ bookscraper/bookscraper/settings.py
@@ -7,6 +7,11 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+from dotenv import dotenv_values
+import os
+
+env_vars = dotenv_values('.env')
+
 BOT_NAME = "bookscraper"
 
 SPIDER_MODULES = ["bookscraper.spiders"]
@@ -16,11 +21,17 @@ FEEDS = {
   BOT_NAME + '/output/booksdata.json' : {'format': 'json', 'overwrite': True}
 }
 
+
+SCRAPEOPS_API_KEY = env_vars.get('SCRAPEOPS_API_KEY')
+SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT = 'https://headers.scrapeops.io/v1/user-agents'
+SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
+SCRAPEOPS_NUM_RESULTS = 5
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "bookscraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -53,9 +64,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "bookscraper.middlewares.BookscraperDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+  #  "bookscraper.middlewares.ScrapeOpsFakeUserAgentMiddleware": 300,
+   "bookscraper.middlewares.ScrapeOpsFakeBrowserHeaderAgentMiddleware": 300,
+  #  "bookscraper.middlewares.BookscraperDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
